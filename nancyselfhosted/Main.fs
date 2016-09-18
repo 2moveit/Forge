9697ad34c3efde91cb24dd5b9d8c122399298a35
Kai <2moveit@users.noreply.github.com>
2016-09-18 12:55:54 +0200

    Corrected output

--- nancyselfhosted/Main.fs
+++ nancyselfhosted/Main.fs
@@ -11,6 +11,6 @@ namespace <%= namespace %>
     let main argv =
         let nancy = new Nancy.Hosting.Self.NancyHost(new Uri(uriString))
         nancy.Start()
-        Console.WriteLine("Nancy is running at %s", uriString)
+        Console.WriteLine("Nancy is running at {0}", uriString)
         while true do Console.ReadLine() |> ignore
         0
